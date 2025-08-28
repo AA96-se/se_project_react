7c7d6116728f9dc75bcade5094ba5a6df4e39277
angel <angelandresarmas@gmail.com>
2025-08-28 01:25:22 -0400

    Added to components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,28 @@
+import logo from "../../assets/Logo.svg";
+import avatar from "../../assets/avatar.svg";
+import "./header.css";
+
 function Header() {
-  return <></>;
+  const now = new Date();
+  const dateStr = now.toLocaleDateString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <header className="header">
+      <img src={logo} alt="WTWR logo" className="header__logo" />
+      <p className="header__place">
+        <time className="header__datetime" dateTime={now}>
+          {dateStr}
+        </time>
+        , New York
+      </p>
+      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <p className="header__username">Terence Tegegne</p>
+      <img src={avatar} alt="user avatar" className="header__avatar" />
+    </header>
+  );
 }
 
 export default Header;
