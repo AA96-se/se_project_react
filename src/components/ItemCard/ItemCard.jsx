ff66062653ff5182c684f3882786f0428f13c72f
angel <angelandresarmas@gmail.com>
2025-08-28 17:42:32 -0400

    added to main card styles

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -2,7 +2,7 @@ function ItemCard({ data }) {
   return (
     <div className="card">
       <h2 className="card__title">{data.name}</h2>
-      <img src={data.link} alt="" className="card__image" />
+      <img src={data.link} alt={data.name} className="card__image" />
     </div>
   );
 }
